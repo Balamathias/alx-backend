adca167b820991f3b34ded45c61a0f395805c9fa
Balamathias <balamathias05@gmail.com>
2024-07-23 20:27:00 +0100

    New commit

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""FIFO Caching implementation
+"""
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """...
+
+    Args:
+        BaseCaching (any): ...
+    """
+
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        """Add item to cache dict
+
+        Args:
+            key (any): The key to insert
+            item (any): the value of Key
+        """
+        if key != None and item != None:
+            if len(self.cache_data.items()) > self.MAX_ITEMS:
+                print(f'DISCARD: {self.cache_data.keys()[0]}')
+                print('\n')
+                self.cache_data.pop(self.cache_data.keys()[0])
+                self.cache_data[key] = item
+
+    def get(self, key):
+        """Get an item from cache by Key
+
+        Args:
+            key (any): Key to get but not None
+
+        Returns:
+            any: The result or None for the given value of key
+        """
+
+        return None if key == None or self.cache_data.get(key) == None \
+            else self.cache_data.get(key)
