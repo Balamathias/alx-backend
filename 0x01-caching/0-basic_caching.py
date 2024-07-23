0251a691b9db32f8f39a8bce0a3d34073154da20
Balamathias <balamathias05@gmail.com>
2024-07-23 19:51:39 +0100

    New commit

---
+++ 0x01-caching/0-basic_caching.py
@@ -0,0 +1,17 @@
+#!/bin/usr/python3
+'''
+Basic caching algorithmic implementation in PYTHON.
+'''
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """Class Implementation of Basic Caching in PYTHON."""
+
+    def put(self, key, item):
+        if key != None and item != None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        return None if key == None else self.cache_data.get(key)
